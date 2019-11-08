69f4c8e83e7c340342aed6ba7092b289ff8adc1c
Armin Bašić <armin.basic@avacom.local>
2019-11-08 23:26:45 +0100

    commit

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Enter = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Word = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.Reset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Enter
+            // 
+            this.Enter.Location = new System.Drawing.Point(354, 45);
+            this.Enter.Name = "Enter";
+            this.Enter.Size = new System.Drawing.Size(112, 32);
+            this.Enter.TabIndex = 0;
+            this.Enter.Text = "Enter";
+            this.Enter.UseVisualStyleBackColor = true;
+            this.Enter.Click += new System.EventHandler(this.Enter_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(107, 52);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(213, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // Word
+            // 
+            this.Word.AutoSize = true;
+            this.Word.Location = new System.Drawing.Point(33, 52);
+            this.Word.Name = "Word";
+            this.Word.Size = new System.Drawing.Size(33, 13);
+            this.Word.TabIndex = 2;
+            this.Word.Text = "Word";
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(107, 123);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(307, 156);
+            this.panel1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(456, 123);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Finish";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(107, 316);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(307, 100);
+            this.panel2.TabIndex = 5;
+            // 
+            // Reset
+            // 
+            this.Reset.Location = new System.Drawing.Point(562, 123);
+            this.Reset.Name = "Reset";
+            this.Reset.Size = new System.Drawing.Size(75, 23);
+            this.Reset.TabIndex = 6;
+            this.Reset.Text = "Reset";
+            this.Reset.UseVisualStyleBackColor = true;
+            this.Reset.Click += new System.EventHandler(this.Reset_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(670, 450);
+            this.Controls.Add(this.Reset);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.Word);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Enter);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Enter;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label Word;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button Reset;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        Label l = new Label();
+        List<string> list = new List<string>();
+        static int result = 0;
+        Label res = new Label();
+
+      
+
+        public Form1()
+        {
+            InitializeComponent();
+          
+        }
+
+        private void Enter_Click(object sender, EventArgs e)
+        {
+            list.Add(textBox1.Text);
+
+            string text = textBox1.Text;
+            l.Size = new Size(100, 100);
+
+            int points = 0;
+
+            if (text.Length == 3 || text.Length == 4)
+            {
+                points = 1;
+                result = result + points;
+            }
+            else if (text.Length == 5)
+            {
+                points = 2;
+                result = result + points;
+            }
+            else if (text.Length == 6)
+            {
+                points = 3;
+                result = result + points;
+            }
+            else if (text.Length == 7)
+            {
+                points = 5;
+                result = result + points;
+            }
+            else if (text.Length >= 8)
+            {
+                points = 11;
+                result = result + points;
+            }
+
+
+            l.Text += textBox1.Text +"  "+ points+ Environment.NewLine;
+            textBox1.Text = string.Empty;
+            panel1.Controls.Add(l);
+
+
+            res.Text = "Your result is " + result;
+            panel2.Controls.Add(res);
+
+           
+         
+
+        }
+
+       
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+            Enter.Enabled = false;
+
+           
+        }
+
+        private void Reset_Click(object sender, EventArgs e)
+        {
+            list.Clear();
+            result = 0;
+            l.Text = "";
+            res.Text = "";
+            Enter.Enabled = true;
+
+        }
+    }
+}
